74d99d0c359954d31550fb236673e8391e7bc248
olayinkakhalid <adewoyekhalid237@gmail.com>
2026-05-13 21:06:18 +0100

    did a new page and some other pages

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+AOS.init();
+
 document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.getElementById("hamburger");
     const navLinks = document.getElementById("navLinks");
+    const bookBtn = document.querySelectorAll(".book-btn");
 
     hamburger.addEventListener("click", () => {
         navLinks.classList.toggle("show");
+    });
+
+    bookBtn.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            window.location.href = 'bookingPage.html';
+        });
     });
 });
