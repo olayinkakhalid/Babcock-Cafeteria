fa2ea17684d53e9bde816ee909de6239868619e7
olayinkakhalid <adewoyekhalid237@gmail.com>
2026-05-13 22:03:51 +0100

    change some nav stuff

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.getElementById("hamburger");
     const navLinks = document.getElementById("navLinks");
     const bookBtn = document.querySelectorAll(".book-btn");
+    const bookingPage = document.querySelector(".bookingpage");
 
     hamburger.addEventListener("click", () => {
         navLinks.classList.toggle("show");
     });
 
-    ulLink.forEach((link) => {
-        link.addEventListener("click", () => {
+    bookingPage.forEach((bookingPage) => {
+        bookingPage.addEventListener("click", () => {
             window.location.href = 'bookingPage.html';
         });
     });
